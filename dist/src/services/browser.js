b6a9b926bd15e5d877a99ed586807b7b164e5d6d
devulapalliSatya <devulapallisatya@outlook.com>
2023-09-08 14:42:56 +0530

    browser changed +++

--- dist/src/services/browser.js
+++ dist/src/services/browser.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -37,28 +14,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.startBrowser = void 0;
 const chrome_aws_lambda_1 = __importDefault(require("chrome-aws-lambda"));
-const puppeteer = __importStar(require("puppeteer"));
-const puppeteer_extra_1 = __importDefault(require("puppeteer-extra"));
-const puppeteer_extra_plugin_stealth_1 = __importDefault(require("puppeteer-extra-plugin-stealth"));
-puppeteer_extra_1.default.use((0, puppeteer_extra_plugin_stealth_1.default)());
+const puppeteer_core_1 = __importDefault(require("puppeteer-core"));
+// import  PuppeteerExtra from "puppeteer-extra";
+// import Stealth from 'puppeteer-extra-plugin-stealth';
+// PuppeteerExtra.use(Stealth());
 function startBrowser() {
     return __awaiter(this, void 0, void 0, function* () {
-        let browser;
         try {
             if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-                console.log("Opening the browser......");
-                const browser = yield puppeteer.launch({
-                    headless: 'new',
+                let browser = yield puppeteer_core_1.default.launch({
                     args: [...chrome_aws_lambda_1.default.args, "--hide-scrollbars", "--disable-web-security"],
                     defaultViewport: chrome_aws_lambda_1.default.defaultViewport,
                     executablePath: yield chrome_aws_lambda_1.default.executablePath,
+                    headless: 'new',
                     ignoreHTTPSErrors: true,
-                    ignoreDefaultArgs: ['--disable-extensions']
                 });
-                const newpage = yield browser.newPage();
-                // Return the browser object here if needed
+                console.log('=====in production=======');
                 return browser;
             }
+            console.log('=====in local=======');
+            let browser = yield puppeteer_core_1.default.launch({});
+            return browser;
+            // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+            //     console.log("Opening the browser......");
+            //     const browser = await puppeteer.launch({
+            //         headless: 'new',
+            //         args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+            //         defaultViewport: chromium.defaultViewport,
+            //         executablePath: await chromium.executablePath,
+            //         ignoreHTTPSErrors: true,
+            //         ignoreDefaultArgs: ['--disable-extensions']
+            //     });
+            // const newpage = await browser.newPage();
+            // // Return the browser object here if needed
+            // return browser;
+            //}
         }
         catch (err) {
             console.log("Could not create a browser instance => : ", err);
--- src/services/browser.ts
+++ src/services/browser.ts
@@ -1,30 +1,47 @@
 import chromium from "chrome-aws-lambda";
-import * as puppeteer from "puppeteer";
-import  PuppeteerExtra from "puppeteer-extra";
-import Stealth from 'puppeteer-extra-plugin-stealth';
+import * as puppeteer1 from "puppeteer";
+import puppeteer from 'puppeteer-core';
+// import  PuppeteerExtra from "puppeteer-extra";
+// import Stealth from 'puppeteer-extra-plugin-stealth';
 
-PuppeteerExtra.use(Stealth());
+// PuppeteerExtra.use(Stealth());
+
 
 export async function startBrowser() {
-    let browser;
-    
+
+
     try {
         if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-            console.log("Opening the browser......");
-            const browser = await puppeteer.launch({
-                headless: 'new',
+            let browser = await puppeteer.launch({
                 args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
                 defaultViewport: chromium.defaultViewport,
                 executablePath: await chromium.executablePath,
+                headless: 'new',
                 ignoreHTTPSErrors: true,
-                ignoreDefaultArgs: ['--disable-extensions']
             });
-            const newpage = await browser.newPage();
-            
-            // Return the browser object here if needed
+            console.log('=====in production=======')
             return browser;
         }
-    }catch (err) {
+        console.log('=====in local=======')
+        let browser = await puppeteer.launch({});
+        return browser;
+
+        // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        //     console.log("Opening the browser......");
+        //     const browser = await puppeteer.launch({
+        //         headless: 'new',
+        //         args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        //         defaultViewport: chromium.defaultViewport,
+        //         executablePath: await chromium.executablePath,
+        //         ignoreHTTPSErrors: true,
+        //         ignoreDefaultArgs: ['--disable-extensions']
+        //     });
+        // const newpage = await browser.newPage();
+
+        // // Return the browser object here if needed
+        // return browser;
+        //}
+    } catch (err) {
         console.log("Could not create a browser instance => : ", err);
     }
 }
